804ec186676f1a83199a9755551fea5cd0b497e3
SB067746 <SB067746@cerner.net>
2019-05-21 17:33:11 +0530

    Added layout files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 
 function App() {
   return (
     <div className="App">
-      <h3>My React App</h3>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
